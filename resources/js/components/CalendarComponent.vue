98c15d242e1b9610ff860b50c792e373c1118527
Jazin Mendoza <jazinmendoza16@gmail.com>
2019-07-15 16:29:01 +0800

    Remove infinite loop error

--- resources/js/components/CalendarComponent.vue
+++ resources/js/components/CalendarComponent.vue
@@ -10,10 +10,9 @@
           </div>
         </div>
       </div>
-      <div  class="row day-row" v-for="(day, dayIndex) in days" :key="dayIndex" :class="{active: day.isActive}">
-        <div class="col-md-6">{{day.date.getDate()}} {{weekday_abbrv[day.weekday]}}</div>
-        <div>{{filterEventM(day, dayIndex)}}</div>
-        <div class="col-md-6">{{day.event.eventName}}</div>
+      <div  class="row day-row" v-for="(day, dayIndex) in days" :key="dayIndex" :class="{active: isDayEmpty(day.event)}">
+        <div class="col-md-6"><h5>{{day.date.getDate()}} {{weekday_abbrv[day.weekday]}}</h5></div>
+        <div class="col-md-6" v-if="isDayEmpty(day.event)">{{day.event.eventName}}</div>
       </div>
   </div>
   </div>
@@ -24,21 +23,25 @@
   
     export default {
         mounted() {
-          this.showDays();
+
           var self = this;
+
           axios.get('api/events').then(function(res){
             var data = res.data;
+
             data.forEach(event => {
               var weekdays=[];
               event.weekdays.forEach(rec => {
                 weekdays.push(rec.weekday);
               })
+
               var event = {
                 eventName: event.name,
                 startDate: new Date(event.start_date),
                 endDate: new Date(event.end_date),
                 weekdaysIndex: weekdays
               }
+
               self.$store.commit('SET_EVENTS', event)
             });
           });
@@ -47,50 +50,34 @@
           return {
             monthAndYear: moment().format('MMM YYYY'),
             weekday_abbrv: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-            months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            days: [],
+            months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
           }
         },
         methods: {
-          showDays() {
-            for(var i=0; i < moment(moment().format('MMM YYYY')).daysInMonth(); i++){
-              this.days.push(
-                {
-                  weekday: moment().startOf('month').add(i, 'days').toDate().getDay(),
-                  date: new Date(moment().startOf('month').add(i, 'days').toDate()),
-                  event: { 
-                    eventName: '',
-                    startDate: '',
-                    endDate: '',
-                    weekdays: []
-                  },
-                  isActive: false
-                });  
-            }
-          },
-          filterEventM(day, dayIndex) {
-            this.events.forEach(event=>{
-              if(day.date >= event.startDate && day.date <= event.endDate 
-              && event.weekdaysIndex.includes(parseInt(day.weekday))){
-                this.days[dayIndex].event = event;
-                this.days[dayIndex].isActive = true;
-                return event.eventName;
-              }
-            })
-          }
-        },
-        filters: {
-           filterEvent(event, day){
-            if(day.date >= event.startDate && day.date <= event.endDate 
-            && event.weekdaysIndex.includes(parseInt(day.weekday))){
-              return event.eventName
-            };
-             
+          isDayEmpty(event){
+            return event!=null;
           }
         },
         computed: {
-          events(){
-            return this.$store.getters.events
+          days(){
+            var days = [];
+            var day, fullDate, weekday;
+
+            for(var i=0; i < moment(moment().format('MMM YYYY')).daysInMonth(); i++){
+
+              day = moment().startOf('month').add(i, 'days').toDate()
+              weekday = day.getDay()
+              fullDate = new Date(day)
+
+              days.push({
+                  weekday: weekday,
+                  date: fullDate,
+                  event: this.$store.getters.getEvent(fullDate, weekday)
+                });  
+
+            }
+
+            return days;
           }
         }
     }
